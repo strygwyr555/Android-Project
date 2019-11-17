f499d91db4db727c93fa389b5a82fe473d446577
Enid Maharjan <46753597+strygwyr555@users.noreply.github.com>
2019-11-16 22:27:45 -0600

    Panic Button

---
+++ PanicButton/PanicButton/app/src/main/java/com/panicRescue/panicbutton/PersonAddNumberDialog.java
@@ -0,0 +1,62 @@
+package com.panicRescue.panicbutton;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.text.InputType;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class PersonAddNumberDialog {
+    private AlertDialog dialog;
+
+    public PersonAddNumberDialog(final Context context, final ListView toUpdate) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        LinearLayout root = new LinearLayout(context);
+        root.setOrientation(LinearLayout.VERTICAL);
+
+        final EditText textName = new EditText(context);
+        textName.setHint("Name");
+
+        final EditText textNumber = new EditText(context);
+        textNumber.setHint("Number");
+        textNumber.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        root.addView(textName);
+        root.addView(textNumber);
+
+        dialog = builder
+                    .setView(root)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String name = textName.getText().toString(), number = textNumber.getText().toString();
+                            if (name != null && !name.equals(""))
+                                if (number != null && !number.equals("") && number.length() == 10) {
+                                    Person p = new Person(number, name);
+                                    PersonManager.savePerson(p, context);
+                                    toUpdate.setAdapter(new PersonAdapter(context, PersonManager.getSavedPersons(context)));
+                                }
+                                else{
+
+                                }
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create();
+    }
+
+    public void show() {
+        dialog.show();
+    }
+}
